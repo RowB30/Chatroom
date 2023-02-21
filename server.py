ac7583ca13be24c928294d430890b467cc4f5f3b
Sean <shroby30@gmail.com>
2023-02-20 20:57:37 -0500

    IP not neede on client side

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from socket import *
 serverPort = 11000
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('127.0.0.1', serverPort))
-serverSocket.listen(1)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(5)
 
 print("Server is ready!")
 while True:
+
     connectionSocket, addr = serverSocket.accept()
     message = connectionSocket.recv(1024).decode()
     capMessage = message.upper()
